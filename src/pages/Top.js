e3b159161279292be1f7376fe4565540c5087f01
knights1210 <jamis.1984@gmail.com>
2020-12-16 14:47:43 +0900

    display lists

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -2,6 +2,8 @@ import React, { useContext, useEffect } from "react";
 import Layout from "../components/Layout/Layout";
 import { fetchPopularData } from "../apis/index";
 import { Store } from "../store/index";
+import { VideoGridItem } from "../components/VideoGridItem/VideoGridItem";
+import { VideoGrid } from "../components/VideoGrid/VideoGrid";
 
 const Top = () => {
   const { globalState, setGlobalState } = useContext(Store);
@@ -9,10 +11,29 @@ const Top = () => {
   useEffect(() => {
     fetchPopularData().then((res) => {
       console.log("data", res);
-      setGlobalState({type: "SET_POPULAR",payload: {popular: res.data.items}})
-  })
+      setGlobalState({
+        type: "SET_POPULAR",
+        payload: { popular: res.data.items },
+      });
+    });
   }, []);
-  return (<Layout>top page</Layout>)
+  return (
+    <Layout>
+      <VideoGrid>
+        {globalState.popular &&
+          globalState.popular.map((popular) => {
+            return (
+              <VideoGridItem
+                id={popular.id}
+                key={popular.id}
+                src={popular.snippet.thumbnails.standard.url}
+                title={popular.snippet.title}
+              />
+            );
+          })}
+      </VideoGrid>
+    </Layout>
+  );
 };
 
 export default Top;
